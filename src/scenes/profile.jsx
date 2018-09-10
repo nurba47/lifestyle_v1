c9b4a320150a2d06df01f1f62d82005f7cc67572
Nurba <nurba47@gmail.com>
2018-09-10 19:34:16 +0600

    profile update

--- src/scenes/profile.jsx
+++ src/scenes/profile.jsx
@@ -27,6 +27,82 @@ class Profile extends React.Component {
             <h5 className="card-text">
               Реферальный код: {profile ? profile.user.referralCode : 0}
             </h5>
+              <div className="table-responsive">
+                  <table className="table">
+                      <tr>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td className="user">
+                              <div>
+                                  <strong>Z.Arzymatova</strong>
+                              </div>
+                          </td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                      </tr>
+                      <tr>
+                          <td></td>
+                          <td className="user">
+                              <div>
+                                  <strong>new profill</strong>
+                              </div>
+                          </td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td className="user">
+                              <div>
+                                  <strong>new profil</strong>
+                              </div>
+                          </td>
+                          <td></td>
+                      </tr>
+                      <tr>
+                          <td className="user">
+                              <div>
+                                  <strong>new profil</strong>
+                              </div>
+                          </td>
+                          <td></td>
+                          <td>
+                              <div>
+                                  <strong> new profil</strong>
+                              </div>
+                          </td>
+                          <td></td>
+                          <td className="user">
+                              <div>
+                                  <strong>new profill</strong>
+                              </div>
+                          </td>
+                          <td></td>
+                          <td>
+                              <div>
+                                  <strong>new profil</strong>
+                              </div>
+                          </td>
+                      </tr>
+                  </table>
+              </div>
+              <select className="option">
+                  <option>100 - Льгота</option>
+                  <option>250 - Льгота</option>
+                  <option>400 - Льгота</option>
+              </select>
+              <table  border="1" cellPadding="8" cellSpacing="0">
+                  <caption>Список вознаграждений</caption>
+                  <tr>
+                      <th colSpan="5" bgcolor="#7fffd4" align="left">Дата</th>
+                      <th>Поступления</th>
+                      <th>Снял</th>
+                  </tr>
+                  <tr onInput={1}><td colSpan="5">10.10.2018</td><td>44</td><td>66</td></tr>
+                  <tr><td colSpan="5">10.10.2018</td><td>45</td><td>55</td></tr>
+                  <tr><td colSpan="5">10.10.2018</td><td>33</td><td>22</td></tr>
+                  <tr><td colSpan="5">10.10.2018</td><td>44</td><td>34</td></tr>
+              </table>
           </div>
         </div>
       </div>
