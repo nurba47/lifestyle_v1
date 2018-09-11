0d7b485b552cd7eb7fdea5d73a3b8faad4a4fa23
Mirlan <m.maksitaliev@gmail.com>
2018-09-11 12:39:38 +0600

    fixed registration

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,7 @@ import { post } from "./index";
 import { LOGIN, USERS } from "../pathes";
 
 export const register = user => {
-  return post(USERS, user);
+  return post(USERS, { user });
 };
 
 export const login = user => {
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,16 +1,18 @@
 import { observable, action } from "mobx";
-import {register, login} from "../api/auth"
+import { register, login } from "../api/auth";
 
 class Auth {
   PROFILE = "profile";
 
-  @observable user;
-  @observable ready;
+  @observable
+  user;
+  @observable
+  ready;
 
   loadProfile() {
     let profile = window.localStorage.getItem(this.PROFILE);
     profile = JSON.parse(profile);
-    
+
     if (profile && profile.user) this.user = profile.user;
     else this.user = null;
     this.ready = true;
@@ -20,9 +22,9 @@ class Auth {
   async login(user) {
     try {
       const response = await login(user);
-      
-      window.localStorage.setItem(this.PROFILE, JSON.stringify({user: response.user}));
-      
+
+      window.localStorage.setItem(this.PROFILE, JSON.stringify({ user: response.user }));
+
       this.loadProfile();
 
       return Promise.resolve();
@@ -36,9 +38,9 @@ class Auth {
   @action.bound
   async register(user) {
     try {
-      const response = await register(user)
+      const response = await register(user);
 
-      window.localStorage.setItem(this.PROFILE, JSON.stringify({user: response.user}));
+      window.localStorage.setItem(this.PROFILE, JSON.stringify({ user: response.user }));
 
       this.loadProfile();
 
