1bbac57ea89d6ecf56d465a88b225f14bbe4a314
Nurba <nurba47@gmail.com>
2018-10-08 20:32:35 +0600

    update

--- src/scenes/marketing.jsx
+++ src/scenes/marketing.jsx
@@ -1,47 +1,20 @@
 import React from 'react'
+import mark from "../images/mark.jpeg";
 export default class MarketingScene extends React.Component {
     render() {
         return(
             <div style={{backgroundColor: '#fff' }}>
                 <div className="container" style={{ padding: '50px 25px', backgroundColor: '#fff' }}>
                     <h1><strong><i>Маркетинг План</i></strong></h1>
-                    <table border="1" cellPadding="8" cellSpacing="0">
-                        <tr>
-                            <th colSpan="5" bgcolor="#7fffd4" align="left">
-                                Вход в баллах
-                                1-Б = 100 y.e.
-                            </th>
-                            <th colSpan="5" bgcolor="#7fffd4">Активность 54 балл</th>
-                            <th colSpan="5" bgcolor="#7fffd4">Продукция при входе и активность</th>
-                            <th colSpan="5" bgcolor="#7fffd4">Выплаты в % ( 1 - Балл = 100 y.e.)</th>
-                            <th colSpan="5" bgcolor="#7fffd4">Бонусы</th>
-                            <th colSpan="5" bgcolor="#7fffd4">Льгота 360-Баллов</th>
-                        </tr>
-                        <tr onInput={1}>
-                            <td colSpan="5">100 Баллов</td>
-                            <td colSpan="5">Каждый месяц (ежемесячно)</td>
-                            <td colSpan="5">5 упаковок Кымдан - 2 при активности 3 упаковки</td>
-                            <td colSpan="5">10% выплаты до 5ого уровня</td>
-                            <td colSpan="5">по 950 Баллов с двух веток  10000 y.e.</td>
-                            <td colSpan="5">Освобождения от активности (активации) и удвоение бонуса (20 000 y.e при ТО 950 баллов с двух веток)</td>
-                        </tr>
-                        <tr>
-                            <td colSpan="5">250 Баллов</td>
-                            <td colSpan="5">Каждый 6 месяц (полугодовое)</td>
-                            <td colSpan="5">13 упаковок Кымдан-2 при активности 3 упаковки Камыдан-2 +1 упаковка в подарок</td>
-                            <td colSpan="5">12% выплаты от личного приглашеннных</td>
-                            <td colSpan="5">по 950 Баллов с двух веток  10000 y.e.</td>
-                            <td colSpan="5">Освобождения от активности (активации) и удвоение бонуса (20 000 y.e при ТО 950 баллов с двух веток)</td>
-                        </tr>
-                        <tr>
-                            <td colSpan="5">400 Баллов</td>
-                            <td colSpan="5">В год 1 раз(годовое)</td>
-                            <td colSpan="5">21 упаковка Кымдан-2 + при активности 3 упаковки  Кымдан-2  +2 упаковки в подарок</td>
-                            <td colSpan="5">15% выплаты от личного приглашенных</td>
-                            <td colSpan="5">по 950 Баллов с двух веток  10000 y.e.</td>
-                            <td colSpan="5">Освобождения от активности (активации) и удвоение бонуса (20 000 y.e при ТО 950 баллов с двух веток)</td>
-                        </tr>
-                    </table>
+                    <div class="img-thumbnail" style={{ padding: "50px 0" }}>
+                        <a href={mark} className="swipebox">
+                            <img src={mark} alt="image1"
+                                 style={{border: '3px solid #167CE9', borderRadius: '8px', margin: '10px 20px'}}
+                                 width="400px" className="img-responsive"/>
+                        </a>
+
+                    </div>
+
                     <h1><u><i><strong>Прайс- Лист</strong></i></u></h1>
                     <table border="1" cellPadding="8" cellSpacing="0">
                         <tr>
